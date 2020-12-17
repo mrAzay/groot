bddec3093217bfb692daabd99cf9246052e9e486
mrAzay <furoxaz@gmail.com>
2020-12-17 21:47:52 +0200

    fix

--- js/modal-order.js
+++ js/modal-order.js
@@ -2,14 +2,25 @@ let modalOrder = document.querySelector('.modal-order')
 let modalOrderButton = document.querySelectorAll('.order-btn')
 let modalOrderClose = document.querySelector('.modal-order-close')
 
-modalOrderButton.forEach(item=>{
-	item.addEventListener('click',()=>{
+modalOrderButton.forEach(item => {
+	item.addEventListener('click', () => {
 		modalOrder.classList.add('active')
 		overlay.classList.add('active')
 	})
 })
 
-modalOrderClose.addEventListener('click', ()=>{
+modalOrderClose.addEventListener('click', () => {
 	modalOrder.classList.remove('active');
 	overlay.classList.remove('active')
 })
+
+// document.querySelector('.modal-push').addEventListener('click', (event) => {
+// 	event.preventDefault();
+// 	axios.get('https://dev.groot.net.ua/en')
+// 		.then(function (response) {
+// 			console.log(response);
+// 		})
+// 		.catch(function (error) {
+// 			console.log(error);
+// 		})
+// })
